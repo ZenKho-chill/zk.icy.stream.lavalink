0868de696dd7f8fd31cbba562723978a5db62d77
ZenKho <heruhoangphuc0601@gmail.com>
2025-10-04 18:10:10 +0700

    v1.0.5 - Add @ComponentScan configuration for proper Spring bean discovery

---
+++ src/main/java/com/zenkho/icy/IcyPluginConfiguration.java
@@ -0,0 +1,9 @@
+package com.zenkho.icy;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.zenkho.icy")
+public class IcyPluginConfiguration {
+}
